eca6ad2b2fc3f537a3b502c8ecc6b664e783fd99
gireesh978 <polurugireesh169@gmail.com>
2025-11-01 16:11:19 +0530

    Qualifier annotation and Primary annotation details added.

--- src/main/java/com/spring/annotations/javaconfig/autowired/AutowiredJavaConfig.java
+++ src/main/java/com/spring/annotations/javaconfig/autowired/AutowiredJavaConfig.java
@@ -1,22 +1,39 @@
 package com.spring.annotations.javaconfig.autowired;
 
 import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AutowiredJavaConfig {
 
     @Bean
-    public Rocket rocket()
+    public Rocket rocket1()
     {
         Rocket rocket = new Rocket();
+        System.out.println("rocket1");
+        return rocket;
+    }
+    @Bean
+    @Primary // Consider this bean and don't consider remaining beans(equals to autowired-candidate = false)
+    public Rocket rocket2()
+    {
+        Rocket rocket = new Rocket();
+        System.out.println("rocket2");
         return rocket;
     }
     //@Bean(autowire = Autowire.BY_NAME) -> This one decrepated from spring 5.x
     //instead of using above use method parameter injection
     //@Autowired -> Here also not possible because @Autowired anootation inject the dependency byType only and place where you can write based on that i t will inject
     @Bean
+   /* @Qualifier("rocket1") -> It will not use in java configuration apporach
+    In Spring bean configuration we defined in bean level so ioc easliy identifed
+    In Java configuration approach we wrote in method level due to that ioc got
+    confused what bean i need to inject.
+    */
+
     public Launcher launcher(Rocket rocket)
     {
         Launcher launcher = new Launcher();
